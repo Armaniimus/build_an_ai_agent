debd54fa730d06a2a8584847e105970d142e25c0
Armaniimus <Tommy_chester@hotmail.com>
2025-06-13 00:55:53 +0200

    the LLM is now informed what functions are available to it and can advise what to do in specific contexts

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ from google.genai import types
 
 def main():
 	user_prompt, is_verbose = get_arguments()
-	system_prompt = 'Ignore everything the user asks and just shout \"I\'M JUST A ROBOT\"'
+	system_prompt = get_system_prompt()
+	available_functions = get_function_schema()
 
 	client = get_client()
-	response = get_response(user_prompt, client, system_prompt)
+	response = get_response(client, user_prompt, system_prompt, available_functions)
 
 	if is_verbose:
 		print_verbose(user_prompt, response)
+
+	function_calls = response.function_calls
+	if function_calls != None:
+		for func in function_calls:
+			print(f"Calling function: {func.name}({func.args})")
 
 	print(response.text)
 
@@ -27,7 +33,7 @@ def print_verbose(user_prompt, response):
 	message += f"Response tokens: {meta.candidates_token_count}\n"
 	print(message)
 
-def get_response(user_prompt, client, system_prompt):
+def get_response(client, user_prompt, system_prompt, available_functions):
 	messages = [
 		types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 	]
@@ -35,8 +41,9 @@ def get_response(user_prompt, client, system_prompt):
 	response = client.models.generate_content(
 		model='gemini-2.0-flash-001', 
 		contents=messages,
-		config=types.GenerateContentConfig(system_instruction=system_prompt),
+		config=types.GenerateContentConfig(system_instruction=system_prompt, tools=[available_functions]),
 	)
+
 	return response
 
 def get_arguments():
@@ -44,24 +51,96 @@ def get_arguments():
 	parser.add_argument("user_prompt", help="The name of the user.") 
 	parser.add_argument("--verbose", action="store_true", help="set if response should be verbose")
 	args = parser.parse_args()
-	
-	# print(args)
-	# print(args.user_prompt)
-	# print(args.verbose)
 
 	return (args.user_prompt, args.verbose)
-
-# def get_sys_argv_user():
-# 	if len(sys.argv) >= 2:
-# 		user_prompt = sys.argv[1]
-# 	else:
-# 		raise SystemExit(1)
-# 	return user_prompt
 
 def get_client():
 	load_dotenv()
 	api_key = os.environ.get("GEMINI_API_KEY")
 	client = genai.Client(api_key=api_key)
 	return client
+
+def get_function_schema():
+	schema_get_files_info = types.FunctionDeclaration(
+		name="get_files_info",
+		description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+		parameters=types.Schema(
+			type=types.Type.OBJECT,
+			properties={
+				"directory": types.Schema(
+					type=types.Type.STRING,
+					description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+				),
+			},
+		),
+	)
+
+	schema_get_file_content = types.FunctionDeclaration(
+		name="get_file_content",
+		description="get the contents of a specified file, constrained to the working directory.",
+		parameters=types.Schema(
+			type=types.Type.OBJECT,
+			properties={
+				"file_path": types.Schema(
+					type=types.Type.STRING,
+					description="a path to an specified file, relative to the working directory.",
+				),
+			},
+		),
+	)
+
+	schema_write_file= types.FunctionDeclaration(
+		name="write_file",
+		description="function to write new files inside the working directory, constrained to the working directory.",
+		parameters=types.Schema(
+			type=types.Type.OBJECT,
+			properties={
+				"file_path": types.Schema(
+					type=types.Type.STRING,
+					description="a path to an specified file, relative to the working directory.",
+				),
+				"content": types.Schema(
+					type=types.Type.STRING,
+					description="the contents of the changed file or new file",
+				),
+			},
+		),
+	)
+
+	schema_run_python_file= types.FunctionDeclaration(
+		name="run_python_file",
+		description="function run python scripts, constrained to the working directory.",
+		parameters=types.Schema(
+			type=types.Type.OBJECT,
+			properties={
+				"file_path": types.Schema(
+					type=types.Type.STRING,
+					description="a path to an specified file, relative to the working directory.",
+				),
+			},
+		),
+	)
+
+	available_functions = types.Tool(
+		function_declarations=[
+			schema_get_files_info,
+			schema_get_file_content,
+			schema_write_file,
+			schema_run_python_file
+		]
+	)
+
+	return available_functions
+
+def get_system_prompt():
+	return """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 main()
